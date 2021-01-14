41f3d147663820154424555bc986f5ef6b7242f9
Joel Krim <krimj@seas.upenn.edu>
2021-01-14 18:44:35 -0500

    fixing stuffs

--- run_pyright.py
+++ run_pyright.py
@@ -1,33 +1,32 @@
 import os
-from typing import Any, Dict, List, cast
+import re
+from typing import Set
 
-import toml
+PYRIGHT_CMD = "pyright ."
+MISSING_TYPESTUB_PATTERN = r'.*error: Stub file not found for "(.*)".*'
 
 
 def run_pyright() -> None:
     """
-    Automatically run pyright on all dependencies
+    Find all missing typestubs, generate them,
+    then run pyright
     """
+    modulesMissingStubs: Set[str] = set()
 
-    tomlDict: Dict[str, Any] = {}
+    for line in os.popen(PYRIGHT_CMD).readlines():
+        match = re.match(MISSING_TYPESTUB_PATTERN, line)
+        if match:
+            group = match.group(1)
+            group = re.sub(r"\..*", "", group)
+            modulesMissingStubs.add(group)
 
-    with open("pyproject.toml", "r") as f:
-        tomlDict = cast(Dict[str, Any], toml.load(f))
+    for module in modulesMissingStubs:
+        cmd = f"{PYRIGHT_CMD} --createstub {module}"
+        print(cmd)
+        os.system(cmd)
 
-    dependencies = cast(
-        List[str],
-        [
-            dep
-            for dep in tomlDict["tool"]["poetry"]["dependencies"].keys()
-            if dep != "python"
-        ],
-    )
-
-    cmd = f"pyright . --createstub {' '.join(dependencies)}"
-
-    print(cmd)
-
-    os.system(cmd)
+    print(PYRIGHT_CMD)
+    os.system(PYRIGHT_CMD)
 
 
 if __name__ == "__main__":
