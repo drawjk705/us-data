2e6f5e06d02f68a3edb60a68fad3b243a1c6ad4a
Joel Krim <drawjk705@gmail.com>
2021-01-22 12:49:58 -0500

    fixes

--- the_census/_utils/timer.py
+++ the_census/_utils/timer.py
@@ -13,10 +13,10 @@ def timer(func: _Func) -> _Func:
 
         end_time = time.perf_counter()
 
-        elapsedMs = (end_time - start_time) * 1000
+        elapsed_ms = (end_time - start_time) * 1000
 
         logging.getLogger(__name__).debug(
-            f"[{func.__qualname__}] - duration: {elapsedMs:.2f}ms"
+            f"[{func.__qualname__}] - duration: {elapsed_ms:.2f}ms"
         )
 
         return retval
